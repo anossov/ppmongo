aaa3230a771f645e250ccac5d54b79cbd44ec81e
Pavel Anossov <anossovp@RMBP.(none)>
2013-04-08 23:18:26 +0400

    add url and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 import codecs
 
 def readme(fn):
@@ -17,6 +17,12 @@ setup(name='ppmongo',
     author='Pavel Anossov',
     author_email='anossov@gmail.com',
 
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 2.7',
+        ],
+
+    url='https://github.com/anossov/ppmongo',
     license='LICENSE.txt',
     description='Retrieve and pretty-print data from mongo',
     long_description=readme('README.txt'),
